82fe46de00010e93e7ea7ba60a818e7b50c8dc46
doubleInc <aalavi@protonmail.com>
2020-04-28 01:56:23 +1000

    Map overlay component added,

---
+++ src/js/components/Maplay.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { css, ClassNames } from "@emotion/core";
+//leaflet
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+// leaflet css file
+import "leaflet/dist/leaflet.css";
+
+// hack so that leaflet's images work after going through webpack
+import marker from "leaflet/dist/images/marker-icon.png";
+import marker2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: marker2x,
+  iconUrl: marker,
+  shadowUrl: markerShadow,
+});
+
+class Mapoverlay extends React.Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  };
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <React.Fragment>
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              Lorem ipsum dolor sit amet, <br /> consectetur adipisicing elit.
+            </Popup>
+          </Marker>
+          <Marker position={[51, -0.08]} />
+        </Map>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Mapoverlay;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,37 +2,13 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { css, ClassNames } from "@emotion/core";
-//leaflet
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-// leaflet css file
-import "leaflet/dist/leaflet.css";
-
-// hack so that leaflet's images work after going through webpack
-import marker from "leaflet/dist/images/marker-icon.png";
-import marker2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: marker2x,
-  iconUrl: marker,
-  shadowUrl: markerShadow,
-});
 
 //
 import Writeup from "../js/components/Writeup";
+import Mapoverlay from "../js/components/Maplay";
 
 class Home extends React.Component {
-  state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13,
-  };
-
   render() {
-    const position = [this.state.lat, this.state.lng];
     return (
       <React.Fragment>
         <Container
@@ -46,18 +22,7 @@ class Home extends React.Component {
             style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
           >
             <Writeup />
-            <Map center={position} zoom={this.state.zoom}>
-              <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={position}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-              <Marker position={[51, -0.08]} />
-            </Map>
+            <Mapoverlay />
           </Typography>
           />
         </Container>
@@ -65,39 +30,5 @@ class Home extends React.Component {
     );
   }
 }
-
-// export default function Home() {
-//   const position = [51.505, -0.09];
-//   const zoom = 13;
-//   return (
-//     <React.Fragment>
-//       <Container fixed>
-//         <Typography
-//           component="div"
-//           style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
-//         >
-//           <Map center={position} zoom={zoom}>
-//             <TileLayer
-//               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//             />
-//             <Marker position={position}>
-//               <Popup>
-//                 A pretty CSS3 popup. <br /> Easily customizable.
-//               </Popup>
-//             </Marker>
-//           </Map>
-//         </Typography>
-//         />
-//       </Container>
-//     </React.Fragment>
-//   );
-// }
-
-// const Home = () => (
-//   <div>
-//     <h2>Welcome</h2>
-//   </div>
-// );
 
 export default Home;
