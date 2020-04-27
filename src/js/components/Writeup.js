d9e4a5f31c7fb22c34d0d4ba64340a85eac7c5a5
doubleInc <aalavi@protonmail.com>
2020-04-28 01:56:23 +1000

    Map overlay component added,

--- src/js/components/Writeup.js
+++ src/js/components/Writeup.js
@@ -2,27 +2,37 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 
 import { css, ClassNames } from "@emotion/core";
+//Styles
+
+const heading = css`
+  padding-top: 10px;
+  font-weight: 100;
+  color: #1b5e20;
+  text-align: center;
+`;
+
+const heroText = css`
+  background-color: #eee;
+  height: 15em;
+`;
 
 const Writeup = () => {
   return (
-    <div
-      css={css`
-        background-color: #eee;
-        height: 15em;
-      `}
-    >
-      <Typography
-        css={css`
-          padding-top: 10px;
-          font-weight: 100;
-          color: #1b5e20;
-          text-align: center;
-        `}
-        variant="h1"
-        component="h2"
-        gutterBottom
-      >
-        h1. Heading
+    <div css={heroText}>
+      <Typography css={heading} variant="h1" component="h2" gutterBottom>
+        Recycle old Tech!
+        <Typography
+          variant="body1"
+          gutterBottom
+          css={css`
+            color: #1b5e20;
+          `}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
       </Typography>
     </div>
   );
