e03cdce7647d05f82b56d67be661ccf1add8c3dd
doubleInc <alialavi@gmail.com>
2020-04-30 12:34:55 +1000

    User login and logout completed.

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Link } from "@reach/router";
+import { Link, navigate } from "@reach/router";
 import { css, ClassNames } from "@emotion/core";
+import { auth, googleAuthProvider } from "../../../firebase";
 
 // dashboard compenent
 import Userdash from "./Userdash";
@@ -22,15 +23,48 @@ import green from "@material-ui/core/colors/green";
 
 class Navbar extends Component {
   state = {
-    loggedIn: true,
+    loggedIn: false,
+    currentUser: null,
   };
 
   static LoggedIn = ({ on, usrimg }) =>
     on ? <Userdash usrimg={usrimg} /> : null;
 
-  static LoggedOut = ({ on }) => (on ? null : <Button>Log In</Button>);
+  static LoggedOut = ({ on }) =>
+    on ? null : (
+      <Button
+        onClick={() => auth.signInWithPopup(googleAuthProvider)}
+        css={css`
+          color: white;
+          border: 1px solid white;
+        `}
+      >
+        Log In
+      </Button>
+    );
+
+  // component update before loading dom
+  componentDidMount = async () => {
+    await auth.onAuthStateChanged((currentUser) => {
+      console.log("AUTH_CHANGE", currentUser, this.state.loggedIn);
+      if (currentUser !== null) {
+        this.setState({
+          currentUser,
+          loggedIn: true,
+        });
+      } else {
+        this.setState({
+          currentUser,
+          loggedIn: false,
+        });
+      }
+    });
+  };
 
   render() {
+    const usrimg = this.state.currentUser
+      ? this.state.currentUser.photoURL
+      : null;
     return (
       <div
         css={css`
@@ -54,7 +88,7 @@ class Navbar extends Component {
             {React.Children.map(this.props.children, (child) =>
               React.cloneElement(child, {
                 on: this.state.loggedIn,
-                usrimg: this.props.usrImage,
+                usrimg: usrimg,
               })
             )}
           </Toolbar>
